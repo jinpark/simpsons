e543480947d27fc9fab857f7f693a166cde22853
Jin Park <jin+github@jinpark.net>
2016-05-07 11:02:59 +0900

    change random method

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,7 @@
 class Video < ActiveRecord::Base
 
     def self.random(count)
-        order("RANDOM()").limit(count)
+        # order("RANDOM()").limit(count)
+        self.all.sample(count)
     end
 end
